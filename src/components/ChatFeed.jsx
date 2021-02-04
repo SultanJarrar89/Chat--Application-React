ac0525861f6de675c8279d2d4d96ea688dec5315
sultan <sultanjarrar89@gmail.com>
2021-02-03 18:05:36 -0800

    implementing ChatFeed

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -4,9 +4,39 @@ import MyMessage from './MyMessage'
 import OtherMessage from './OtherMessage'
 
 function ChatFeed(props) {
+  const { chats, activeChat, userName, messages } = props
+  const chat = chats && chats[activeChat]
+
+  const renderMessages = () => {
+    const keys = Object.keys(messages)
+    keys.map((key, idx) => {
+      const message = messages[key]
+      const lastMessageKey = idx === 0 ? null : keys[idx - 1]
+      const isMyMessage = userName === message.sender.userName
+      return (
+        <div key={`msg_${idx}`} style={{ width: '100%' }}>
+          <div className='message-block'>
+            {isMyMessage ? <MyMessage /> : <OtherMessage />}
+          </div>
+          <div
+            className='read-receipts'
+            style={{
+              marginRight: isMyMessage ? '18px' : '0px',
+              marginLeft: isMyMessage ? '0px' : '68px',
+            }}
+          >
+            read-receipts
+          </div>
+        </div>
+      )
+    })
+  }
+  if (!chat) return 'Loading...'
   return (
-    <div>
-      <h1>hi</h1>
+    <div className='chat-feed'>
+      <div className='chat-title-container'>
+        <div className='chat-title'>{chat?.title}</div>
+      </div>
     </div>
   )
 }
