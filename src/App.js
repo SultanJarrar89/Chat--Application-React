26b83a360c30ff4cb5f708faec28da1da4077fd5
sultan <sultanjarrar89@gmail.com>
2021-02-08 16:39:17 -0800

    LoginForm

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { ChatEngine } from 'react-chat-engine'
 import ChatFeed from './components/ChatFeed'
+import LoginForm from './components/LoginForm'
 import './App.css'
 
 function App() {
+  if (!localStorage.getItem('username')) return <LoginForm />
   return (
     <ChatEngine
       height='100vh'
       projectID={process.env.REACT_APP_PROJECT_ID}
-      userName='sultan89'
-      userSecret={process.env.REACT_APP_USER_SEC}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
   )
